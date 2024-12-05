05c64e9379420f75fcb8bc43a67f6dfe8653c075
Levy Jardim <levy.jardim02@gmail.com>
2024-12-05 05:06:56 -0300

    inserindo novas informações no front-end

--- frontend/listadetarefas/src/App.jsx
+++ frontend/listadetarefas/src/App.jsx
@@ -2,17 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios"
 
 function App() {
+  const [nome, setNome] = useState();
+  const [email, setEmail] = useState();
+  const [fone, setTarefas] = useState();
+  const [data_nascimento, setData] = useState();
+
+  function handleAddUser(e){
+    e.preventDefault();
+
+    axios.post("http://localhost:3333/user",{
+      nome,
+      email,
+      fone,
+      data_nascimento
+    })
+    .then((response)=>{
+      console.log("Cadastro realizado")
+    })
+    .catch((erro)=>{
+      console.log("Errooooo")
+    })
+    
+  }
 
   return (
-    <form action="">
-      <input type="text" />      
-      <input type="text" />      
-      <input type="text" />      
-      <input type="text" />    
-      <button type=''>salvar</button>  
-    </form>
+    <>
+      <form onSubmit={handleAddUser}>
+        <input type="text" onChange={e=>setNome(e.target.value)}/>
+        <input type="text" onChange={e=>setEmail(e.target.value)}/>
+        <input type="text" onChange={e=>setTarefas(e.target.value)}/>
+        <input type="text" onChange={e=>setData(e.target.value)}/>
+        <button type='submit'>Cadastrar</button>
+      </form>
+    </>
   )
 }
 
