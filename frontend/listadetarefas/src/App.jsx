a3055b92e358295ca0f2aa93065aa53cd978fe63
Levy Jardim <levy.jardim02@gmail.com>
2024-12-05 04:51:05 -0300

    inserindo estrutura do front-end

---
+++ frontend/listadetarefas/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <form action="">
+      <input type="text" />      
+      <input type="text" />      
+      <input type="text" />      
+      <input type="text" />    
+      <button type=''>salvar</button>  
+    </form>
+  )
+}
+
+export default App
